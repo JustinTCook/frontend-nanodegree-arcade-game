ddda8ea77436b1d3e0c55d8e6d99239508610981
Justin Cook <jtcook19@gmail.com>
2018-02-24 19:14:18 -0700

    ES6 classes

--- js/app.js
+++ js/app.js
@@ -1,34 +1,57 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+  constructor(x, y){
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.sprite = 'image/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+  }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt){
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor(sprite = 'image/char-boy.png'){
+    this.sprite = sprite;
+  }
+
+  // Update the player's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt){
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+  }
+
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput(keyPress){
+
+  }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+const allEnemies = [new Enemy(), new Enemy()];
 // Place the player object in a variable called player
+const player = new Player();
 
 
 
