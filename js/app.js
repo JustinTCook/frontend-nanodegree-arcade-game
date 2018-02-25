65706b2eddb407ea075b5503eaafaf0857e2f0fc
Justin Cook <jtcook19@gmail.com>
2018-02-24 20:07:45 -0700

    Player movement

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
+// Possible board positions
+const xPositions = [200];
+const yPositions = [];
+
 // Enemies our player must avoid
 class Enemy {
   constructor(x, y){
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.sprite = 'image/enemy-bug.png';
+    this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
   }
@@ -25,8 +29,10 @@ class Enemy {
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-  constructor(sprite = 'image/char-boy.png'){
+  constructor(sprite = 'images/char-boy.png'){
     this.sprite = sprite;
+    this.x = 202;
+    this.y = 320;
   }
 
   // Update the player's position, required method for game
@@ -42,7 +48,21 @@ class Player {
   }
 
   handleInput(keyPress){
+    var horizontal = 101;
+    var vertical = 83;
 
+    if(keyPress == 'left' && this.x > 0) {
+        this.x -= horizontal;
+    }
+    if(keyPress == 'right' && this.x < 400) {
+        this.x += horizontal;
+    }
+    if(keyPress == 'up' && this.y > 3) {
+        this.y -= vertical;
+    }
+    if(keyPress == 'down' && this.y < 400) {
+        this.y += vertical;
+    }
   }
 }
 
